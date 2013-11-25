5498e00583958e0fa73ae849aa8f5ae8dbf8ad43
Ben Nelson <nelson.blaha@gmail.com>
2013-11-24 22:01:44 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bitcoinbounty::Application.config.secret_key_base = 'e265c60177eed81e261e59989f0463cce3f09beab4f6ec88aa67844ac831d8de94d17826132407c1fa9a68ad8670309299cc9be3499ec08d81317c8e2d28d154'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bitcoinbounty::Application.config.session_store :cookie_store, key: '_bitcoinbounty_session'
